37437f6581727692cc389bc3d26bcf895ea2e0ed
종박 <78944379+jongpark1234@users.noreply.github.com>
2022-09-19 22:24:47 +0900

    Nav, Footer Update

--- src/components/Footer/index.style.jsx
+++ src/components/Footer/index.style.jsx
@@ -1,7 +1,7 @@
 import styled from 'styled-components'
 export const background = styled.div`
     width: 100vw;
-    height: 300px;
+    height: 200px;
     background-color: darkgray;
     box-sizing: border-box;
 `
--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -2,7 +2,6 @@ import * as style from './index.style'
 const Render = () => {
     return (
         <style.background>
-            <span>메인페이지</span>
         </style.background>
     )
 }
